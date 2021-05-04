465e7e6671e0396d9b0272d99571329a32ef4a4f
Isaac <mendez.vovinam@gmail.com>
2021-05-04 16:43:15 +0200

    Save before leaving class

--- src/be/bxl/formation/models/Duree.java
+++ src/be/bxl/formation/models/Duree.java
@@ -27,38 +27,61 @@ public class Duree {
     //endregion
 
     //region Setters
-    public void setJours(int jours) {
+
+    private void setJours(int jours) {
         this.jours = jours;
     }
 
-    public void setHeures(int heures) {
+    private void setHeures(int heures) {
         this.heures = heures;
     }
 
-    public void setMinutes(int minutes) {
+    private void setMinutes(int minutes) {
         this.minutes = minutes;
     }
 
-    public void setSecondes(int secondes) {
+    private void setSecondes(int secondes) {
         this.secondes = secondes;
     }
     //endregion
 
+
     //region Constructors
     public Duree(int nbSecondes){
-        setSecondes(nbSecondes);
+        int nbMinutes, nbHeures;
+        this.setSecondes(nbSecondes%60);
+        nbMinutes=(nbSecondes-this.getSecondes())/60;
+        this.setMinutes(nbMinutes%60);
+        nbHeures = (nbMinutes-this.getMinutes())/60;
+        this.setHeures(nbHeures%24);
+        this.setJours(nbHeures/24);
     }
 
     public Duree(int jour, int heure, int minute, int seconde){
-        setJours(jour);
-        setHeures(heure);
-        setMinutes(minute);
-        setSecondes(seconde);
+
     }
     //endregion
 
     //region Methodes
+    public String getDuree (){
+        String message =this.getJours()+" jours "+this.getHeures()+" heures "+this.getMinutes()+" minutes "
+                +this.getSecondes()+" secondes ";
+        return message;
+    }
 
+    public int getTotalSeconde(){
+        int totalSec;
+        totalSec = (getJours()*86400)+(getHeures()*360)+(getMinutes()*60)+getSecondes();
+        return totalSec;
+    }
+
+    public void subDuree(Duree aSoustraire){
+
+    }
+
+    public void addDuree(Duree aAjouter){
+
+    }
     //endregion
 
 }
