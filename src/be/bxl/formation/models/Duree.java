aea0f8bc92b68de526a4086e850333c06e04a402
Isaac <mendez.vovinam@gmail.com>
2021-05-05 08:45:59 +0200

    Make all work only with total seconds

--- src/be/bxl/formation/models/Duree.java
+++ src/be/bxl/formation/models/Duree.java
@@ -2,79 +2,46 @@ package be.bxl.formation.models;
 
 public class Duree {
 
-    private int jours;
-    private int heures;
-    private int minutes;
-    private int secondes;
-
-    //region Getters
-
-    public int getJours() {
-        return jours;
-    }
-
-    public int getHeures() {
-        return heures;
-    }
-
-    public int getMinutes(){
-        return minutes;
-    }
-
-    public int getSecondes() {
-        return secondes;
-    }
-    //endregion
-
-    //region Setters
-
-    private void setJours(int jours) {
-        this.jours = jours;
-    }
-
-    private void setHeures(int heures) {
-        this.heures = heures;
-    }
-
-    private void setMinutes(int minutes) {
-        this.minutes = minutes;
-    }
-
-    private void setSecondes(int secondes) {
-        this.secondes = secondes;
-    }
-    //endregion
-
+    private int totalSeconde;
 
     //region Constructors
-    public Duree(int nbSecondes){
-        int nbMinutes, nbHeures;
-        this.setSecondes(nbSecondes%60);
-        nbMinutes=(nbSecondes-this.getSecondes())/60;
-        this.setMinutes(nbMinutes%60);
-        nbHeures = (nbMinutes-this.getMinutes())/60;
-        this.setHeures(nbHeures%24);
-        this.setJours(nbHeures/24);
+    public Duree(int nbSeconde) {
+        this.totalSeconde = nbSeconde;
     }
 
-    public Duree(int jour, int heure, int minute, int seconde){
+    public Duree(int jour, int heure, int minute, int seconde) {
+        if(jour < 0 || heure >= 24 || heure < 0 || minute >= 60 || minute < 0 || seconde >= 60 || seconde < 0) {
+            throw new IllegalArgumentException("Durée invalide !");
+        }
 
+        this.totalSeconde = (jour * 86400) + (heure * 3600) + (minute * 60) + seconde;
     }
     //endregion
 
+    //region Getters
+    public int getTotalSeconde() {
+        return totalSeconde;
+    }
+
+    public int getJour() {
+        return totalSeconde/86400;
+    }
+
+    public int getHeure() {
+        return (totalSeconde/3600) % 24;
+    }
+
+    public int getMinute() {
+        return (totalSeconde/60) % 60;
+    }
+
+    public int getSeconde() {
+        return totalSeconde % 60;
+    }
+    //endregion
+
+
     //region Methodes
-    public String getDuree (){
-        String message =this.getJours()+" jours "+this.getHeures()+" heures "+this.getMinutes()+" minutes "
-                +this.getSecondes()+" secondes ";
-        return message;
-    }
-
-    public int getTotalSeconde(){
-        int totalSec;
-        totalSec = (getJours()*86400)+(getHeures()*360)+(getMinutes()*60)+getSecondes();
-        return totalSec;
-    }
-
     public void subDuree(Duree aSoustraire){
 
     }
